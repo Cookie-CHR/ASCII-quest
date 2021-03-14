81cd41d2501598ed63aa4acc7c4e3bcf64a826a1
Cookie-CHR <pacchetto99@gmail.com>
2021-03-14 17:46:05 +0100

    ⚙️ Added instructions

--- Godot/Generic/general.gd
+++ Godot/Generic/general.gd
@@ -11,15 +11,15 @@ func guess_scn(dest, minutes):
 	# Find the right scene for the current time (useful for long scenes)	
 	var dest_array = []
 	if dest == "Forest":
-		dest_array = ["08:10", "09:10"]
+		dest_array = ["08:10", "09:00","13:00"]
 	elif dest == "Tavern":
-		dest_array = ["08:10"]
+		dest_array = ["08:00", "09:00", "10:00", "12:00", "14:00"]
 	elif dest == "Park":
-		dest_array = ["08:00","08:20","08:30"]
+		dest_array = ["08:00","08:20","08:30", "09:00", "12:00", "13:00", "19:00"]
 	elif dest == "Castle":
-		dest_array = ["08:10"]
+		dest_array = ["08:00", "09:00", "13:00", "14:00", "18:00", "19:00"]
 	elif dest == "Lab":
-		dest_array = ["08:10"]
+		dest_array = ["08:00", "09:00", "10:00", "11:00", "12:00", "14:00","17:00", "18:00"]
 	
 	var closest_time = Time.findClosest(dest_array, minutes)
 	return "%02d:00 - %02d:00/%02d_%02d_%s" % [closest_time[0], closest_time[0]+1, closest_time[0], closest_time[1], dest]
