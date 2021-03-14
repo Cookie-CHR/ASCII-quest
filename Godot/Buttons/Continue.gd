042a5cc21c84af08e14a6903bca8c0c0526da51d
Cookie-CHR <pacchetto99@gmail.com>
2021-03-14 12:11:01 +0100

    ⚙️ New script

---
+++ Godot/Buttons/Continue.gd
@@ -0,0 +1,20 @@
+extends "Improved_Button.gd"
+
+func re_init():
+	# Re-initialize the variables
+	# Useful for destinations that require an updated curr_sc, or the button's name
+	
+	# First, we have to find out the current scene and its index
+	var scene = get_tree().current_scene.filename
+	scene = str(scene.replace(".tscn", ""))
+	var lastNum = scene.substr(len(scene)-1, 1)
+	# The 1st scene doesn't have a numeric index, so we'll assume any non-int last digit represents the 1st scene
+	if int(lastNum) == 0:
+		lastNum = 2
+	else:
+		#else, we update the index and remove the "_X" from previous scene
+		scene.erase(len(scene)-2, 2)# 
+		lastNum = int(lastNum)+1
+	
+	#now, we combine the basic scene name with a "_" and our new index
+	self.dest = scene+"_"+str(lastNum)
--- Godot/Buttons/Hunt.gd
+++ Godot/Buttons/Hunt.gd
@@ -1,8 +1,8 @@
-extends Button
+extends "Improved_Button.gd"
 
 func re_init():
 	# Re-initialize the variables
 	# Useful for destinations that require an updated curr_sc, or the button's name
-	self.dest = "08_10_Forest_2"
+	self.dest = Time.path()+"08_10_Forest_2"
 	self.minutes = 60
 	self.inv_get = "cat"
