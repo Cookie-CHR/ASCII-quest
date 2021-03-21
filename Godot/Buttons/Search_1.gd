bd4aa587181d166bd8070e73e04503460eef5c03
Cookie-CHR <pacchetto99@gmail.com>
2021-03-21 12:37:26 +0100

    ⚙️ Two new scenes, both incomplete

---
+++ Godot/Buttons/Search_1.gd
@@ -0,0 +1,8 @@
+extends "Improved_Button.gd"
+
+func re_init():
+	# Re-initialize the variables
+	# Useful for destinations that require an updated curr_sc, or the button's name
+	self.dest = "13:00 - 14:00/13_00_Forest_Search_1"
+	self.minutes = 60
+	self.music = "res://Music/Forest/Midnight Tale.ogg"
