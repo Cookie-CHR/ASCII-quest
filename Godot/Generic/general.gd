6885c23cf16eeba6c8f5e278909ba9ca7aa280f1
Cookie-CHR <pacchetto99@gmail.com>
2021-03-28 11:52:50 +0200

    ⚙️ Small changes to the scene added yesterday

--- Godot/Generic/general.gd
+++ Godot/Generic/general.gd
@@ -36,7 +36,7 @@ func guess_scn(dest, minutes):
 	elif dest == "Castle":
 		dest_array = ["08:00", "09:00", "13:00", "14:00", "18:00", "19:00"]
 	elif dest == "Lab":
-		dest_array = ["08:00", "09:00", "10:00", "11:00", "12:00", "14:00","17:00", "18:00"]
+		dest_array = ["08:00", "09:00", "11:00", "12:00", "14:00","17:00", "18:00"]
 	
 	var closest_time = Time.findClosest(dest_array, minutes)
 	return "Scenes/%02d:00 - %02d:00/%02d_%02d_%s" % [closest_time[0], closest_time[0]+1, closest_time[0], closest_time[1], dest]
