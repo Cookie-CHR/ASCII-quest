12228186f07433f21c3d8e70615e55536234be28
Cookie-CHR <pacchetto99@gmail.com>
2021-03-31 12:51:07 +0200

    ⚙️  Two new scenes

---
+++ Godot/Buttons/Order_Choice.gd
@@ -0,0 +1,20 @@
+extends "Improved_Button.gd"
+
+func re_init():
+	# Associate each item with its price with a dictionary
+	var items = {"Bread":1,\
+				 "Soup":2,\
+				 "Tea":1,\
+				 "Beer":2,\
+				 "Wine":3
+				}
+	
+	# Check if the item ordered is in the list (should always be true)
+	# and if the player can afford it
+	if (self.name in items.keys() and Inventory.money >= items[self.name]):
+		self.dest = "Scenes/Tavern/Order_"+self.name
+	else:
+		dest = "Scenes/Tavern/Order_Fail"
+	
+	self.minutes = 30
+	self.money = -(items[self.name])
