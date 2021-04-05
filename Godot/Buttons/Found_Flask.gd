15760e5a92ee5621e29b9a686503778ab47456f7
Cookie-CHR <pacchetto99@gmail.com>
2021-04-05 12:05:56 +0200

    ⚙️  New scene

--- Godot/Buttons/Found_Flask.gd
+++ Godot/Buttons/Found_Flask.gd
@@ -3,6 +3,6 @@ extends "Improved_Button.gd"
 func re_init():
 	# Re-initialize the variables
 	# Useful for destinations that require an updated curr_sc, or the button's name
-	self.dest = "Scenes/Forest/Search"
+	self.dest = "Scenes/Forest/13_00"
 	self.minutes = 30
-	self.sc_forbidden = ["Flask_Found"]
+	self.inv_get = ["empty flask"]
