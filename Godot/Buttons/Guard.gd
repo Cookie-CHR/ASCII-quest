36769af95c062b0cc6a3ab6a84adef71ad34ebaf
Cookie-CHR <pacchetto99@gmail.com>
2021-04-06 11:19:16 +0200

    ⚙️  And other small changes I already forgot

--- Godot/Buttons/Guard.gd
+++ Godot/Buttons/Guard.gd
@@ -3,5 +3,5 @@ extends "Improved_Button.gd"
 func re_init():
 	# Re-initialize the variables
 	# Useful for destinations that require an updated curr_sc, or the button's name
-	self.dest = "Scenes/Forest/13_00_Guard"
+	self.dest = "Scenes/Forest/Guard"
 	self.minutes = 30
--- Godot/Buttons/Hunt_Cat.gd
+++ Godot/Buttons/Hunt_Cat.gd
@@ -3,6 +3,6 @@ extends "Improved_Button.gd"
 func re_init():
 	# Re-initialize the variables
 	# Useful for destinations that require an updated curr_sc, or the button's name
-	self.dest = "Scenes/Forest/08_10_Cat"
+	self.dest = "Scenes/Forest/Cat"
 	self.minutes = 60
 	self.inv_get = ["cat"]
