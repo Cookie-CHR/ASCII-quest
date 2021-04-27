c3a363f6d94dd52e800e8fa984de7fecb865e746
Cookie-CHR <pacchetto99@gmail.com>
2021-04-27 13:17:24 +0200

    ⚙️ New function

---
+++ Godot/Buttons/Win_Or_Lose.gd
@@ -0,0 +1,10 @@
+extends "Improved_Button.gd"
+
+func re_init():
+	# Re-initialize the variables
+	# Useful for destinations that require an updated curr_sc, or the button's name
+	if (Time.h < 14):
+		self.dest = "Scenes/Lab/Fail"
+	else:
+		self.dest = "Scenes/Lab/Secret_Find"
+	
