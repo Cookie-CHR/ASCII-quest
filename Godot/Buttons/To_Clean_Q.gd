afd23caa78917a9ae07a3aae64ec4c9f17450bd5
Cookie-CHR <pacchetto99@gmail.com>
2021-04-27 13:12:23 +0200

    🕑 Accepting quests takes 10 minutes now

--- Godot/Buttons/To_Clean_Q.gd
+++ Godot/Buttons/To_Clean_Q.gd
@@ -4,5 +4,6 @@ func re_init():
 	# Re-initialize the variables
 	# Useful for destinations that require an updated curr_sc, or the button's name
 	self.dest = "Scenes/Park/Quests_Clean"
+	self.minutes = 10
 	self.inv_get = ["broom", "gloves"]
 	self.sc_forbidden = ["Clean_Accepted"]
--- Godot/Buttons/To_Seed_Q.gd
+++ Godot/Buttons/To_Seed_Q.gd
@@ -4,5 +4,7 @@ func re_init():
 	# Re-initialize the variables
 	# Useful for destinations that require an updated curr_sc, or the button's name
 	self.dest = "Scenes/Park/Quests_Seed"
+	self.minutes = 10
 	self.inv_get = ["seed"]
 	self.sc_forbidden = ["Seed_Accepted"]
+
