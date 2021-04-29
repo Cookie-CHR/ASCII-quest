fb9fc89548c148aa2c35dbff108170d015b85dbc
Cookie-CHR <pacchetto99@gmail.com>
2021-04-29 11:58:32 +0200

    ⚙️ New button

---
+++ Godot/Buttons/To_S_Room.gd
@@ -0,0 +1,11 @@
+extends "Improved_Button.gd"
+
+func re_init():
+	# Re-initialize the variables
+	# Useful for destinations that require an updated curr_sc, or the button's name
+	self.sc_required = ["S_Room_Found"]
+	
+	if General.sc_find(["Truth_heard"]):
+		self.dest = "Scenes/Lab/S_Room"
+	else:
+		self.dest = "Scenes/Lab/Alert"
