533d485b60b070125ef9672445bbc442ae6e6586
Cookie-CHR <pacchetto99@gmail.com>
2021-04-30 11:40:19 +0200

    ❗️ Fixed typos and small problems

--- Godot/Buttons/Apologize.gd
+++ Godot/Buttons/Apologize.gd
@@ -5,4 +5,4 @@ func re_init():
 	# Useful for destinations that require an updated curr_sc, or the button's name
 	self.dest = "Scenes/"+General.curr_sc+"/Apologize"
 	self.minutes = 10
-	self.sc_required = ["busted"]
+	self.sc_required = ["Busted"]
