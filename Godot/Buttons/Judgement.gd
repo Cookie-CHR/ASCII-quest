56435254b9b63245d24f2d85ea8f12398ab51f5f
Cookie-CHR <pacchetto99@gmail.com>
2021-05-06 11:04:46 +0200

    ⚙️ Forgot a button here

---
+++ Godot/Buttons/Judgement.gd
@@ -0,0 +1,10 @@
+extends "Improved_Button.gd"
+
+func re_init():
+	# Re-initialize the variables
+	# Useful for destinations that require an updated curr_sc, or the button's name
+	if (General.sc_find(["Flask_Proved"]) and General.sc_find(["Guard_Proved"])):
+		self.dest = "Scenes/Castle/Judge_Guilty"
+	else:
+		self.dest = "Scenes/Castle/Judge_Innocent"
+	
