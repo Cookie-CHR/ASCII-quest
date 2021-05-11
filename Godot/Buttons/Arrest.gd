87c7bd94c76e9d5f1ebcb0ff07b2b60e666a320c
Cookie-CHR <pacchetto99@gmail.com>
2021-05-11 11:27:33 +0200

    ⚙️ Renamed a script
    I'll have to reorganize these names...

--- Godot/Buttons/Arrest.gd
+++ Godot/Buttons/Arrest.gd
@@ -4,9 +4,4 @@ func re_init():
 	# Re-initialize the variables
 	# Useful for destinations that require an updated curr_sc, or the button's name
 	self.dest = "Scenes/Castle/Arrest"
-	self.minutes = 120
-	# Need either wine or beer
-	if Inventory.inv_find(["beer"]):
-		self.inv_required = ["beer"]
-	else:
-		self.inv_required = ["wine"]
+	self.minutes = 180
---
+++ Godot/Buttons/Guard_Alcol.gd
@@ -0,0 +1,12 @@
+extends "Improved_Button.gd"
+
+func re_init():
+	# Re-initialize the variables
+	# Useful for destinations that require an updated curr_sc, or the button's name
+	self.dest = "Scenes/Castle/Guard_Alcol"
+	self.minutes = 120
+	# Need either wine or beer
+	if Inventory.inv_find(["beer"]):
+		self.inv_required = ["beer"]
+	else:
+		self.inv_required = ["wine"]
