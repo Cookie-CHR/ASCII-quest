032bf15166d498b005b1d01fbc9a6b0caa3d18ad
Cookie-CHR <pacchetto99@gmail.com>
2021-05-16 23:11:07 +0200

    ⚙️ Time patches
    Can now trigger default ending (which I haven't written yet)

--- Godot/Generic/time_patch.gd
+++ Godot/Generic/time_patch.gd
@@ -5,3 +5,12 @@ func _ready():
 	   General.sc_find(["Cat_Tavern"]) and \
 	   Time.h < 10:
 		get_tree().change_scene("res://Scenes/Tavern/10_00.tscn")
+
+	if not General.sc_find(["Truth_Heard"]) and \
+	   not General.sc_find(["Announced"]) and \
+	   Time.h >= 18:
+		get_tree().change_scene("res://Scenes/X_Else/Announcement.tscn")
+	if General.curr_sc == "Park" and \
+	   not General.sc_find(["Truth_Heard"]) and \
+	   Time.h >= 19:
+		get_tree().change_scene("res://Scenes/Ending/Default.tscn")
