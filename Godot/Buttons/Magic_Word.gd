152413ea6e761614eb340bbbf383f44138914c37
Cookie-CHR <pacchetto99@gmail.com>
2021-09-20 12:30:38 +0200

    ⚙️ Oops, forgot to remove a print statement

--- Godot/Buttons/Magic_Word.gd
+++ Godot/Buttons/Magic_Word.gd
@@ -3,7 +3,7 @@ extends "Improved_Button.gd"
 func re_init():
 	# Re-initialize the variables
 	# Useful for destinations that require an updated curr_sc, or the button's name
-	print ( self.name, General.sc_find([self.name]) )
+	
 	if General.sc_find([self.name]):
 		self.dest = "Scenes/Lab/Alert_Success"
 	else:
