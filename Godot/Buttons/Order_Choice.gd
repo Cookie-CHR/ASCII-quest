408f7f7a5621a943bd3008f012441d90591a7227
Cookie-CHR <pacchetto99@gmail.com>
2022-03-07 13:33:16 +0100

    ⚙️ Changed objects to lowercase

--- Godot/Buttons/Order_Choice.gd
+++ Godot/Buttons/Order_Choice.gd
@@ -2,17 +2,17 @@ extends "Improved_Button.gd"
 
 func re_init():
 	# Associate each item with its price with a dictionary
-	var items = {"Chicken":3,\
-				 "Soup":2,\
-				 "Tea":1,\
-				 "Beer":2,\
-				 "Wine":3
+	var items = {"chicken":3,\
+				 "soup":2,\
+				 "tea":1,\
+				 "beer":2,\
+				 "wine":3
 				}
 	
 	# Check if the item ordered is in the list (should always be true)
 	# and if the player can afford it
 	if (self.name in items.keys() and Inventory.money >= items[self.name]):
-		self.dest = "Scenes/Tavern/Order_"+self.name
+		self.dest = "Scenes/Tavern/Order_"+self.name.capitalize()
 		self.money = -(items[self.name])
 		self.inv_get = [self.name]
 	else:
