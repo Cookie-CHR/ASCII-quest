b28d6cfd3efdcaae6ccecf44d776c8670d16af25
Cookie-CHR <pacchetto99@gmail.com>
2022-03-14 11:15:08 +0100

    ⚙️ Linked old scene to new

--- Godot/Buttons/Glitch_Wine.gd
+++ Godot/Buttons/Glitch_Wine.gd
@@ -3,8 +3,6 @@ extends "Improved_Button.gd"
 func re_init():
 	# Re-initialize the variables
 	# Useful for destinations that require an updated curr_sc, or the button's name
-	if Inventory.money >= 2:
-		self.dest = "Scenes/Tavern/Glitch_Wine_Good"
-	else:
-		self.dest = "Scenes/Tavern/Glitch_Wine_Bad"
+	
+	self.dest = "Scenes/Tavern/Glitch_Wine"
 	self.minutes = 10
