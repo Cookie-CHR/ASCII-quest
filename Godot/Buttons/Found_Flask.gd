2e474cd9cab77fad03ac5c1548329ffe32c733f5
Cookie-CHR <pacchetto99@gmail.com>
2022-03-23 11:51:30 +0100

    ⚙️ Small plot changes
    Now, instead of a random empty flask, in the forest you find the container lost by the wizard

--- Godot/Buttons/Found_Flask.gd
+++ Godot/Buttons/Found_Flask.gd
@@ -5,4 +5,4 @@ func re_init():
 	# Useful for destinations that require an updated curr_sc, or the button's name
 	self.dest = "Scenes/Forest/13_00"
 	self.minutes = 30
-	self.inv_get = ["empty flask"]
+	self.inv_get = ["empty glass"]
