1d9cd38ea79e3fac7b5fc327ab7b109890d16cd4
Cookie-CHR <pacchetto99@gmail.com>
2022-03-24 11:30:38 +0100

    🐱 Small edit to the cat subquest

--- Godot/Buttons/Hunt_Cat.gd
+++ Godot/Buttons/Hunt_Cat.gd
@@ -3,6 +3,6 @@ extends "Improved_Button.gd"
 func re_init():
 	# Re-initialize the variables
 	# Useful for destinations that require an updated curr_sc, or the button's name
-	self.dest = "Scenes/Forest/Hunt_Cat"
+	self.dest = "Scenes/Forest/09_00_Cat"
 	self.minutes = 60
 	self.inv_get = ["cat"]
--- Godot/Buttons/Hunt_N.gd
+++ Godot/Buttons/Hunt_N.gd
@@ -1,7 +1,10 @@
 extends "Improved_Button.gd"
 
 func re_init():
-	# Re-initialize the variables
-	# Useful for destinations that require an updated curr_sc, or the button's name
-	self.dest = "Scenes/Forest/Hunt_N"
-	self.minutes = 60
+	if General.sc_find(["Got_Cat"]):
+		self.dest = "Scenes/Forest/Hunt_N"
+		self.minutes = 60
+	else:
+		self.dest = "Scenes/Forest/09_00_Cat"
+		self.minutes = 60
+		self.inv_get = ["cat"]
