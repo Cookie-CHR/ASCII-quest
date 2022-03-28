f559c45fd81a5d857a9f4065bceab454f2d2c09d
Cookie-CHR <pacchetto99@gmail.com>
2022-03-28 12:42:38 +0200

    ⚙️ Refactoring and renaming

--- Godot/Buttons/Glitch_Glass.gd
+++ Godot/Buttons/Glitch_Glass.gd
@@ -5,7 +5,7 @@ func re_init():
 	# Useful for destinations that require an updated curr_sc, or the button's name
 	self.dest = "Scenes/Tavern/Glitch_Glass"
 	self.minutes = 30
-	self.inv_required = ["magic glass"]
-	self.inv_remove = ["magic glass"]
+	self.inv_required = ["empty glass"]
+	self.inv_remove = ["empty glass"]
 	self.inv_get = ["glitch"]
 	self.sc_forbidden = ["Got_Glitch"]
--- Godot/Buttons/To_Q_Heard.gd
+++ Godot/Buttons/To_Q_Heard.gd
@@ -4,4 +4,3 @@ func re_init():
 	# Re-initialize the variables
 	# Useful for destinations that require an updated curr_sc, or the button's name
 	self.dest = "Scenes/Lab/Q_Heard"
-	self.sc_forbidden = ["T_Group"]
