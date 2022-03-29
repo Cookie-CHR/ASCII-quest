cf16aca24344144fe28420643b193ba79e20e9b1
Cookie-CHR <pacchetto99@gmail.com>
2022-03-29 11:08:12 +0200

    ⚙️ Small changes
    Linked old scene to new, added inventory item I forgot, changed dialogue text

--- Godot/Buttons/To_Q_Forest.gd
+++ Godot/Buttons/To_Q_Forest.gd
@@ -3,6 +3,5 @@ extends "Improved_Button.gd"
 func re_init():
 	# Re-initialize the variables
 	# Useful for destinations that require an updated curr_sc, or the button's name
-	self.dest = "Scenes/Lab/Q_Timmy"
-	self.sc_required = ["T_Group"]
-	self.inv_get = ["empty glass"]
+	self.dest = "Scenes/Lab/Q_Forest"
+	self.sc_required = ["Flask_Found"]
