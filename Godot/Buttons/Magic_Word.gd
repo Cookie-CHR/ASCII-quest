c4f620c64fb9ea358c4cc4b1bde52b4dcf5d500a
Cookie-CHR <pacchetto99@gmail.com>
2022-04-01 11:10:41 +0200

    ⚙️ Made it easier to remember the password

--- Godot/Buttons/Magic_Word.gd
+++ Godot/Buttons/Magic_Word.gd
@@ -5,6 +5,7 @@ func re_init():
 	# Useful for destinations that require an updated curr_sc, or the button's name
 	
 	if General.sc_find([self.name]):
+		self.text += "<--(The word from the wizard's notes)"
 		self.dest = "Scenes/Lab/Alert_Success"
 	else:
 		self.dest = "Scenes/Lab/Alert_Fail"
