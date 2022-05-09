01aa243931b67a841564487ccf19e52cac0b5d52
Cookie-CHR <pacchetto99@gmail.com>
2022-05-09 11:10:43 +0200

    ⚙️ Relinking to epilogue

---
+++ Godot/Buttons/To_Epilogue_Default.gd
@@ -0,0 +1,7 @@
+extends "Improved_Button.gd"
+
+func re_init():
+	# Re-initialize the variables
+	# Useful for destinations that require an updated curr_sc, or the button's name
+	self.dest = "Scenes/X_Epilogue/Default"
+	# time must be 19:00
---
+++ Godot/Buttons/To_Epilogue_You.gd
@@ -0,0 +1,6 @@
+extends "Improved_Button.gd"
+
+func re_init():
+	# Re-initialize the variables
+	# Useful for destinations that require an updated curr_sc, or the button's name
+	self.dest = "Scenes/X_Epilogue/You"
